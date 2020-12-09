12ec3c995bbca80ba0f2b5b7a1359b5bcb57c5ca
gyhandxy <907531706@qq.com>
2020-12-09 14:21:06 +0800

    Using boringssl instead of securetransport

--- chromium/config/Chrome/mac/x64/libavformat/protocol_list.c
+++ chromium/config/Chrome/mac/x64/libavformat/protocol_list.c
@@ -2,7 +2,7 @@ static const URLProtocol *url_protocols[] = {
     &ff_file_protocol,
     &ff_http_protocol,
     &ff_https_protocol,
-    &ff_tls_securetransport_protocol,
+    &ff_tls_openssl_protocol,
     &ff_pipe_protocol,
     &ff_tcp_protocol,
     NULL 
